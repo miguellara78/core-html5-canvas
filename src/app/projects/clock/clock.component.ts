3edd138ae53887c2e9f85595f2d87efe36e14656
Miguel <miguellara@igeekstudios.com>
2017-06-15 15:03:09 -0800

    Refactoring...

--- src/app/projects/clock/clock.component.ts
+++ src/app/projects/clock/clock.component.ts
@@ -13,7 +13,7 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 export class ClockComponent implements OnInit {
   @ViewChild('theCanvas') theCanvas: ElementRef;
 
-  private mainCanvas;
+  private canvas;
   private context: CanvasRenderingContext2D;
 
   private FONT_HEIGHT = 15;
@@ -27,13 +27,13 @@ export class ClockComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.mainCanvas = this.theCanvas.nativeElement;
-    this.context = this.mainCanvas.getContext('2d');
+    this.canvas = this.theCanvas.nativeElement;
+    this.context = this.canvas.getContext('2d');
     this.context.font = this.FONT_HEIGHT + 'px Arial';
-    this.HAND_TRUNCATION =  this.mainCanvas.width/25;
-    this.HOUR_HAND_TRUNCATION = this.mainCanvas.width/10;
+    this.HAND_TRUNCATION =  this.canvas.width/25;
+    this.HOUR_HAND_TRUNCATION = this.canvas.width/10;
     this.NUMERAL_SPACING = 20;
-    this.RADIUS = this.mainCanvas.width / 2 - this.MARGIN;
+    this.RADIUS = this.canvas.width / 2 - this.MARGIN;
     this.HAND_RADIUS = this.RADIUS + this.NUMERAL_SPACING;
     setInterval(()=>{this.drawClock();},1000);
   }
@@ -42,7 +42,7 @@ export class ClockComponent implements OnInit {
 
   drawCircle() {
   this.context.beginPath();
-  this.context.arc(this.mainCanvas.width/2,this.mainCanvas.height/2,this.RADIUS,0,Math.PI*2,true);
+  this.context.arc(this.canvas.width/2,this.canvas.height/2,this.RADIUS,0,Math.PI*2,true);
   this.context.stroke();
 }
 
@@ -55,23 +55,23 @@ export class ClockComponent implements OnInit {
   numerals.forEach(function (numeral) {
     angle = Math.PI/6 * (numeral -3);
     numeralWidth = self.context.measureText(numeral.toString()).width;
-    self.context.fillText(numeral.toString(),self.mainCanvas.width/2 + Math.cos(angle)*(self.HAND_RADIUS)- numeralWidth/2,
-      self.mainCanvas.height/2 + Math.sin(angle)*(self.HAND_RADIUS)+ self.FONT_HEIGHT/3);
+    self.context.fillText(numeral.toString(),self.canvas.width/2 + Math.cos(angle)*(self.HAND_RADIUS)- numeralWidth/2,
+      self.canvas.height/2 + Math.sin(angle)*(self.HAND_RADIUS)+ self.FONT_HEIGHT/3);
   });
 }
 
   drawCenter() {
     this.context.beginPath();
-    this.context.arc(this.mainCanvas.width/2,this.mainCanvas.height/2,5,0,Math.PI*2,true);
+    this.context.arc(this.canvas.width/2,this.canvas.height/2,5,0,Math.PI*2,true);
     this.context.fill();
 }
 
   drawHand(loc, isHour) {
   let angle = (Math.PI*2)*(loc/60) - Math.PI/2,
     handRadius = isHour?this.RADIUS - this.HAND_TRUNCATION - this.HOUR_HAND_TRUNCATION:this.RADIUS-this.HAND_TRUNCATION;
-    this.context.moveTo(this.mainCanvas.width/2,this.mainCanvas.height/2);
-    this.context.lineTo(this.mainCanvas.width/2 + Math.cos(angle) * handRadius,
-      this.mainCanvas.width/2 + Math.sin(angle) * handRadius);
+    this.context.moveTo(this.canvas.width/2,this.canvas.height/2);
+    this.context.lineTo(this.canvas.width/2 + Math.cos(angle) * handRadius,
+      this.canvas.width/2 + Math.sin(angle) * handRadius);
     this.context.stroke();
 }
 
@@ -85,7 +85,7 @@ export class ClockComponent implements OnInit {
 }
 
   drawClock() {
-    this.context.clearRect(0, 0, this.mainCanvas.width, this.mainCanvas.height);
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.drawCircle();
     this.drawNumeral();
--- src/app/projects/home/home.component.ts
+++ src/app/projects/home/home.component.ts
@@ -1,14 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  template: `
+    <canvas #theCanvas width="500" height="500">
+      Canvas is not supported on this device...
+    </canvas>
+    <hr>
+    <label #xPosLabel>X: </label><br>
+    <label #yPosLabel>Y: </label>
+  `,
+  styles: []
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('theCanvas') theCanvas;
+  @ViewChild('xPosLabel') xPosLabel;
+  @ViewChild('yPosLabel') yPosLabel;
+
+  private canvas: HTMLCanvasElement;
+  private context: CanvasRenderingContext2D;
+  private xPositionLabel: HTMLLabelElement;
+  private yPositionLabel: HTMLLabelElement;
 
   constructor() { }
 
   ngOnInit() {
+    this.canvas = this.theCanvas.nativeElement;
+    this.xPositionLabel = this.xPosLabel.nativeElement;
+    this.yPositionLabel = this.yPosLabel.nativeElement;
+    this.context = this.canvas.getContext('2d');
+    this.context.fillStyle = '#cccccc';
+    this.context.fillRect(0,0,this.canvas.width,this.canvas.height);
+    this.canvas.addEventListener('mousemove',(event)=>{this.showMessage(event);});
   }
 
+  showMessage(e: MouseEvent){
+    let x = e.clientX - Math.floor(this.canvas.getBoundingClientRect().left) ;
+    let y = e.clientY - Math.floor(this.canvas.getBoundingClientRect().top);
+    this.xPositionLabel.innerText = 'X: ' + x;
+    this.yPositionLabel.innerText = 'Y: ' + y;
+    this.context.fillRect(0,0,this.canvas.width,this.canvas.height);
+    this.drawHorizontalLine(y);
+    this.drawVerticalLine(x);
+  }
+
+  drawHorizontalLine(verticalPosition: number){
+    let leftBound = 0;
+    let rigthBound = this.canvas.width;
+    this.context.beginPath();
+    this.context.moveTo(leftBound,verticalPosition);
+    this.context.lineTo(rigthBound,verticalPosition);
+    this.context.stroke();
+  }
+
+  drawVerticalLine(horizontalPosition: number){
+    let topBound = 0;
+    let bottomBound = this.canvas.height;
+    this.context.beginPath();
+    this.context.moveTo(horizontalPosition,topBound);
+    this.context.lineTo(horizontalPosition,bottomBound);
+    this.context.stroke();
+  }
 }
